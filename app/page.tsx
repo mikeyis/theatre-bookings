cc58533527f0d74ccb2b75965a679cbaba5f31a5
mikeyis <44427223+mikeyis@users.noreply.github.com>
2024-11-01 23:14:25 +1100

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { TheatreBookings } from './theatre-bookings'
-
-export default async function Page() {
-  const bookings = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/bookings`).then(res => res.json())
-  
-  return <TheatreBookings initialBookings={bookings} />
-}
